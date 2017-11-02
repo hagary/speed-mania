da9bc428904703f45028ac1eb10ee1af6ae02022
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-03 01:31:59 +0200

    move forward auto done :tada:

--- Assets/Scripts/Car_Move.cs
+++ Assets/Scripts/Car_Move.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Car_Move : MonoBehaviour {
-	public float rlSpeed = 1f;
+	public float rlSpeed = 3f;
+	public float fSpeed = 3f;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,8 +12,17 @@ public class Car_Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
+		 * Horizontal Movement (Input)
+		 */
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		moveHorizontal = moveHorizontal * rlSpeed * Time.deltaTime;
 		transform.Translate (moveHorizontal, 0, 0);
+
+		/*
+		 * Forward Movement (Automatic)
+		 */
+		float moveForward = rlSpeed * Time.deltaTime;
+		transform.Translate (0, 0, moveForward);
 	}
 }
