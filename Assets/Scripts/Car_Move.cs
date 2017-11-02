2adc9bf46798ab2479a4fe4653e0f65c21eaafc3
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-03 01:21:51 +0200

    right left movement + other fixes

---
+++ Assets/Scripts/Car_Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Move : MonoBehaviour {
+	public float rlSpeed = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		moveHorizontal = moveHorizontal * rlSpeed * Time.deltaTime;
+		transform.Translate (moveHorizontal, 0, 0);
+	}
+}
