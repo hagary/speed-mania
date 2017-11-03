4a7a19b94228ab6ad950ee227d3bfb45126f9fc3
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-03 15:20:15 +0200

    renamed floor to prefab + infinite road

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+	public float translateZ = 50f;
+	public GameObject road;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			//Move road forward in z-direction to create infinite road illusion.
+			road.transform.Translate(0, 0, translateZ * Time.deltaTime);
+		}
+	}
+}
