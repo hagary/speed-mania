ef8caa3af97d738d3667acb699f175f3155703d7
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-05 15:55:55 +0200

    destroy passed objects done, fixes #1

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+	public float distTolerance = 10f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GameObject car = GameObject.FindGameObjectWithTag ("Player");
+		if (car.transform.position.z > transform.position.z + distTolerance)
+			Destroy (gameObject);
+	}
+}
