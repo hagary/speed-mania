95276b3d78e4a249afa0021cd073f0e0cbaebf1d
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-06 14:38:06 +0200

    pause and resume by esc and button done

---
+++ Assets/Scripts/Pause_Control.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Control : MonoBehaviour
+{
+	public Transform pauseCanvas;
+	bool paused;
+	// Use this for initialization
+	void Start ()
+	{
+		paused = false;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Pause ();
+		}
+	}
+
+	public void Pause ()
+	{
+		paused = !paused;
+		pauseCanvas.gameObject.SetActive (paused);
+		Time.timeScale = paused ? 0 : 1;
+	}
+}
